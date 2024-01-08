97c6ead840ce7ec75ae7ebbf611f869de0b01cd6
Devash30 <devanshshah301@gmail.com>
2024-01-08 21:54:00 +0530

    Updates

---
+++ precache-manifest.bcd9587531e8f45631c8078112885337.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22239f39bbf3bbd51524797a1bf34222",
+    "url": "/React-Project/index.html"
+  },
+  {
+    "revision": "2e889bc7481af63416f0",
+    "url": "/React-Project/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "e76adebc39768961f02a",
+    "url": "/React-Project/static/js/2.b67673e5.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/React-Project/static/js/2.b67673e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7681a10ee04b5f4aa8c",
+    "url": "/React-Project/static/js/3.c2f7971d.chunk.js"
+  },
+  {
+    "revision": "2e889bc7481af63416f0",
+    "url": "/React-Project/static/js/main.2b074f2a.chunk.js"
+  },
+  {
+    "revision": "b1609b5619a850563007",
+    "url": "/React-Project/static/js/runtime-main.4a46fe2c.js"
+  }
+]);
